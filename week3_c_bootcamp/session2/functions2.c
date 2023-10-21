ad363874b021cd6adf25e2f1b5c4f4e3cfaeacb2
Naomi Josebashvili <96797525+naomiJosebashvili@users.noreply.github.com>
2023-10-21 22:15:17 +0000

    Week 3 session 2

---
+++ week3_c_bootcamp/session2/functions2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+float sum (float a, float b)
+{
+    float answer;
+    answer = a + b;
+    return answer;
+}
+
+int main () 
+{
+    float y = 2;
+    float z = sum(5, y); // calling our new function
+
+    printf ("The sum of 5 and %.2f is %.2f\n", y, z);
+}
